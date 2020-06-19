62bb09e8ffedb2fdd40e037bf2c42ad2f9c7b0ff
emagnusson88 <emag39@gmail.com>
2020-06-19 18:50:18 -0400

    Restructuring and cleaning

---
+++ .ipynb_checkpoints/helpers-checkpoint.py
@@ -0,0 +1,151 @@
+import pandas as pd
+import numpy as np
+import scipy.stats
+from sklearn.metrics import mean_squared_error
+import datetime
+import time
+
+
+#summary functions
+def summarize(df, quant=False):
+    summary = pd.DataFrame(df.dtypes,columns=['dtypes'])
+    summary = summary.reset_index()
+    summary['name'] = summary['index']
+    summary = summary[['name','dtypes']]
+    summary['missing'] = df.isnull().sum().values
+    summary['unique'] = df.nunique().values
+    summary['first_val'] = df.loc[0].values
+    summary['last_val'] = df.loc[len(df)-1].values
+    if quant:
+        summary['max'] = df.max().values
+        summary['mean'] = df.mean().values
+        summary['median'] = df.median().values
+        summary['stdev'] = df.std().values
+    for name in summary['name'].value_counts().index:
+        summary.loc[summary['name'] == name, 'entropy'] = round(scipy.stats.entropy(df[name].value_counts(normalize=True), base=2),2)
+    return summary
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+    n = len(data)
+    # x-data for the ECDF: x
+    x = np.sort(data)
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+    return x, y
+
+def combine_df(df):
+    combined = []
+    for x in df:
+        combined+=(list(df[x].values))
+    return combined
+
+#feature engineering functions
+
+def date_cyc_enc(df, col, max_vals):
+    """
+    make time cyclical
+    """
+    df[col + '_sin'] = np.sin(2 * np.pi * df[col]/max_vals)
+    df[col + '_cos'] = np.cos(2 * np.pi * df[col]/max_vals)
+    return df
+
+road_encoding = {
+    'Road': 1,
+    'Street': 2,
+    'Avenue': 2,
+    'Drive': 3,
+    'Broad': 3,
+    'Boulevard': 4
+}
+
+def road_encode(x):
+    if pd.isna(x):
+        return 0
+    for road in road_encoding.keys():
+        if road in x:
+            return road_encoding[road]
+    return 0
+
+
+directions = {
+    'N': 0,
+    'NE': 1/4,
+    'E': 1/2,
+    'SE': 3/4,
+    'S': 1,
+    'SW': 5/4,
+    'W': 3/2,
+    'NW': 7/4
+}
+
+
+monthly_rainfall = {'Atlanta1': 5.02, 'Atlanta5': 3.95, 'Atlanta6': 3.63, 'Atlanta7': 5.12,
+                    'Atlanta8': 3.67, 'Atlanta9': 4.09,'Atlanta10': 3.11, 'Atlanta11': 4.10,
+                    'Atlanta12': 3.82, 'Boston1': 3.92, 'Boston5': 3.24, 'Boston6': 3.22,
+                    'Boston7': 3.06, 'Boston8': 3.37, 'Boston9': 3.47, 'Boston10': 3.79,
+                    'Boston11': 3.98, 'Boston12': 3.73, 'Chicago1': 1.75, 'Chicago5': 3.38,
+                    'Chicago6': 3.63, 'Chicago7': 3.51, 'Chicago8': 4.62, 'Chicago9': 3.27,
+                    'Chicago10': 2.71,  'Chicago11': 3.01, 'Chicago12': 2.43,
+                    'Philadelphia1': 3.52, 'Philadelphia5': 3.88, 'Philadelphia6': 3.29,
+                    'Philadelphia7': 4.39, 'Philadelphia8': 3.82, 'Philadelphia9':3.88 ,
+                    'Philadelphia10': 2.75, 'Philadelphia11': 3.16, 'Philadelphia12': 3.31}
+
+#what though
+def reduce_mem_usage(df, verbose=True):
+    """
+    no idea what this does
+    """
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                c_prec = df[col].apply(lambda x: np.finfo(x).precision).max()
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max and c_prec == np.finfo(np.float16).precision:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max and c_prec == np.finfo(np.float32).precision:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+#root mean squared error
+
+def importance_df(model, features):
+    importances = list(model.feature_importances_)
+    # List of tuples with variable and importance
+    feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(features, importances)]
+    # Sort the feature importances by most important first
+    feature_importances = sorted(feature_importances, key = lambda x: x[1], reverse = True)
+    # Print out the feature and importances
+    return pd.DataFrame(feature_importances, columns = ['feature', 'importance'])
+
+def run_model(model, x_train, x_test, y_train, y_test):
+    start_time = time.time()
+
+    model.fit(x_train, y_train)
+    preds = model.predict(x_test)
+
+    total_time = time.time()-start_time
+    score = np.sqrt(mean_squared_error(preds, y_test))
+
+    return {
+    'run_time':str(datetime.timedelta(seconds = total_time)),
+    'run_score': score
+    }
--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # Geotab-Intersection Kaggle Competition
-
-### Contributors: Erik Magnusson & Devon Navon
-
-Predicting intersection congestion in Atlanta, Boston, Chicago, and Philadelphia 
+Predicting intersection congestion in Atlanta, Boston, Chicago, and Philadelphia
 
 #### Submitted RMSE = 67
 Best competition RMSE = 60, Worst competition RMSE = 132
+
+## Project Structure
+```
+geotab-intersection
+│   README.md
+│   helper.py -frequently used methods
+│   import, visualization, and modeling.ipynb -full project, from cleaning and feature engineering to competition output
+│   model_iteration.ipynb -showcases the model iteration process
+│   BigQuery, PySpark, and H2O -contains notebooks making use of each technology for various modeling steps
+│
+└───data
+│   │   ../geotab-data/test.csv
+│   │   ../geotab-data/train.csv
+│
+└───full process
+│   │   import, visualization, and modeling.ipynb
+│ 
+└───output
+│   │   Submitted RMSE = 67 (Best competition RMSE = 60, Worst competition RMSE = 132)
+│   │ 
